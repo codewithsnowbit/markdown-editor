5346f0849615ec5f7490e4a11ca27fe9c3004c08
SnowBit <dtshah24022006@gmail.com>
2021-09-06 18:53:49 +0530

    Added Files

---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/Heading.css'
+
+class Heading extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 className="text">Markdown Editor</h1>
+            </div>
+        )
+    }
+}
+export default Heading;
---
+++ src/MainContent.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { sampleText } from './SampleText'
+import './styles/MainContent.css'
+import marked from 'marked';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/vibrant-ink.css';
+
+class MainContent extends React.Component{
+    state = {
+        text: sampleText
+      }
+    
+      handleChange = event => {
+        const text = event.target.value
+        this.setState({ text })
+      }
+      renderText = text => {
+        const __html = marked(text, { sanitize: false })
+        return { __html }
+      }
+      
+    render(){
+        return(
+            <div className='container'>
+            <div className='row'>
+                <div className='col-sm-6'>
+                {/* <CodeMirror
+                    onChange={this.handleChange}
+                    lazyLoadMode={false}
+                    options={{
+                        theme: 'vibrant-ink',
+                        tabSize: 2,
+                        keyMap: 'sublime',
+                        mode: 'markdown',
+                    }} /> */}
+                <textarea
+                    onChange={this.handleChange}
+                    value={this.state.text}
+                    className='form-control bg-dark text-light'
+                    rows='35' />
+                </div>
+                <div className='col-sm-6 border bg-dark text-light'>
+                <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+                </div>
+            </div>
+            </div>
+        )
+    }
+}
+
+
+export default MainContent;
